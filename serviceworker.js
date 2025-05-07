4f8446f30d0cd270314061e9274b5939f314f463
Brian Koser <brian@koser.us>
2025-05-07 10:55:29 -0500

    Revert to html promises

--- serviceworker.js
+++ serviceworker.js
@@ -84,34 +84,32 @@ addEventListener('fetch', fetchEvent => {
     // When the user requests an HTML file
     if (request.headers.get('Accept').includes('text/html')) {
         fetchEvent.respondWith(
-            async function() {
-                try {
-                    const fetchResponse = await fetch(request);
+            fetch(request)
+            .then( fetchResponse => {
+                // Cache page
+                const copy = fetchResponse.clone();
+                fetchEvent.waitUntil(
+                    caches.open(pagesCacheName)
+                    .then( pagesCache => {
+                        return pagesCache.put(request, copy);
+                    })
+                );
+                return fetchResponse;
+            })
+            // If not retrievable, display the offline page
+            .catch(error => {
+                console.error(error);
 
-                    // Cache page
-                    const copy = fetchResponse.clone();
-                    fetchResponse.waitUntil(
-                        async function() {
-                            const pagesCache = await caches.open(pagesCacheName);
-                            return pagesCache.put(request, copy);
-                        }
-                    );
-                    return fetchResponse;
-                }
-                catch (error) {
-                    console.error(error);
-
-                    const cacheResponse = await caches.match(request);
-                    
+                return caches.match(request)
+                .then( cacheResponse => {
                     if (cacheResponse) {
                         return cacheResponse;
                     }
 
                     return caches.match('/offline.html');
-                }
-            }
+                })
+            })
         );
-
         return;
     }
 
